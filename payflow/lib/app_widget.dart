ad9216ac02a36d3d6ee5d8b266f793074880bc0c
Alan Vieira Martins <alanhorcrux@gmail.com>
2021-11-18 09:53:41 -0300

    criando tela barcodeScanner

--- payflow/lib/app_widget.dart
+++ payflow/lib/app_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:payflow/modules/barcode_scanner/barcode_scanner_page.dart';
 import 'package:payflow/modules/login/login_page.dart';
 import 'package:payflow/modules/splash/splash_page.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
@@ -12,12 +13,14 @@ class AppWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         title: 'Pay Flow',
-        theme: ThemeData(primaryColor: AppColors.primary),
+        theme: ThemeData(
+            primaryColor: AppColors.primary, primarySwatch: Colors.orange),
         initialRoute: "/splash",
         routes: {
           "/splash": (context) => SplashPage(),
           "/home": (context) => HomePage(),
-          "/login": (context) => LoginPage()
+          "/login": (context) => LoginPage(),
+          "/barcode_scanner": (context) => BarcodeScannerPage()
         });
   }
 }
